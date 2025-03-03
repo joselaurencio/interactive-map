fd2ac4b3c722123a75f84da5317afddf759b54de
RoboCodexYT <118416395+RoboCodexYT@users.noreply.github.com>
2025-03-03 16:12:15 -0600

    Update script.js
    Added Markers with cool popups about key places in China & Mongolia

--- script.js
+++ script.js
@@ -6,7 +6,15 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© OpenStreetMap contributors'
 }).addTo(map);
 
-// Add a marker
-var marker = L.marker([29.4241, -98.4936]).addTo(map)
-    .bindPopup("Hello, San Antonio!")
-    .openPopup();
+// Add markers with popups
+var beijing = L.marker([39.9042, 116.4074]).addTo(map)
+    .bindPopup("<b>Beijing, China 🇨🇳</b><br>Capital of China. Home to the Great Wall & Forbidden City.");
+
+var shanghai = L.marker([31.2304, 121.4737]).addTo(map)
+    .bindPopup("<b>Shanghai, China 🌆</b><br>China's largest city and financial hub.");
+
+var ulanbator = L.marker([47.9186, 106.9176]).addTo(map)
+    .bindPopup("<b>Ulaanbaatar, Mongolia 🇲🇳</b><br>Capital of Mongolia, famous for its cold winters and nomadic culture.");
+
+var gobiDesert = L.marker([42.5, 105.0]).addTo(map)
+    .bindPopup("<b>Gobi Desert 🏜️</b><br>A vast desert spanning China & Mongolia, known for its extreme climate.");
